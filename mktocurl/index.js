f67f32f2f03ce882b812875149fcbd901399a05b
Marc Hassan <mhassan@schoology.com>
2017-06-13 13:08:28 -0400

    add logging

--- mktocurl/index.js
+++ mktocurl/index.js
@@ -7,14 +7,22 @@ var request = require('request');
 var Transform = require('stream').Transform;
 var json2csv = require('json2csv');
 var refreshAccessToken = require('./utils').refreshAccessToken;
+var winston = require('winston');
+var _ = require('lodash');
+
+var logger = new (winston.Logger)({transports: [new (winston.transports.Console)({timestamp: true, level: 'debug'})]});
+
+logger.info('Starting');
 
 var access_token = 'none';
 try {
     access_token = fs.readFileSync('access_token.txt').toString();
 } catch (ignore) {}
 
-var nextPageToken;
-var stream = new Readable({objectMode:true});
+logger.info('Current Access Token: ' + access_token);
+
+var nextPageToken = process.argv[2];
+var stream = new Readable({objectMode: true});
 stream._read = function () {};
 
 var fields = fs.readFileSync('./fields.txt').toString().split("\n").splice(1);
@@ -29,101 +37,97 @@ async.doWhilst(
             baseUrl += '&nextPageToken=' + nextPageToken;
         }
 
-		var currEndField = 0;
-		var currFields = [];
 		var outerResults = [];
-		async.whilst(
-            function () {
-                currFields = fields.slice(currEndField, currEndField + maxFieldsPerCall);
-                currEndField += maxFieldsPerCall;
-                return !!currFields.length;
-            },
-            function (innerCb) {
-                var url = baseUrl + '&access_token=' + access_token + '&fields=' + currFields.join(',');
-                doGet(url, function (err, results, token) {
-                    if (err) {
-                        innerCb(err);
-                    } else {
-                        nextPageToken = token;
-                        results = results.map(function (result) {
-                            var stripped = {};
-                            Object.keys(result).forEach(function (k) {
-                                if (result[k]) {
-                                    stripped[k] = result[k];
-                                }
-                            });
-                            return stripped;
-                        });
-                        if (!outerResults.length) {
-                            outerResults = results;
-                        } else {
-                            results.forEach(function (result) {
-                                var joinResult = outerResults.find(function (outerResult) {
-                                    return outerResult.id === result.id;
-                                });
-                                if (joinResult) {
-                                    Object.keys(result).forEach(function (k) {
-                                        joinResult[k] = result[k];
-                                    });
-                                }
-                            });
-                        }
-                        innerCb();
-                    }
-                });
-            }, function (err) {
+		async.each(_.chunk(fields, maxFieldsPerCall), function (chunk, eachCb) {
+            var url = baseUrl + '&access_token=' + access_token + '&fields=' + chunk.join(',');
+            doGet(url, function (err, results, token) {
                 if (err) {
-                    cb(err);
+                    eachCb(err);
                 } else {
-                    outerResults.forEach(function (result) {
-                        stream.push(result);
+                    nextPageToken = token;
+                    results = results.map(function (result) {
+                        var stripped = {};
+                        Object.keys(result).forEach(function (k) {
+                            if (result[k]) {
+                                stripped[k] = result[k];
+                            }
+                        });
+                        return stripped;
                     });
-                    cb();
+                    if (!outerResults.length) {
+                        outerResults = results;
+                    } else {
+                        results.forEach(function (result) {
+                            var joinResult = outerResults.find(function (outerResult) {
+                                return outerResult.id === result.id;
+                            });
+                            if (joinResult) {
+                                Object.keys(result).forEach(function (k) {
+                                    joinResult[k] = result[k];
+                                });
+                            }
+                        });
+                    }
+                    eachCb();
                 }
+            });
+        }, function (err) {
+            if (err) {
+                cb(err);
+            } else {
+                outerResults.forEach(function (result) {
+                    stream.push(result);
+                });
+                cb();
             }
-        );
+        });
 	},
 	function () { return !!nextPageToken; },
 	function (err) {
 		stream.push(null);
-		if (err) console.error(err);
-		console.log('done');
+		if (err) logger.error(err);
+		logger.info('Done');
 	}
 );
 
-function doGet(url, callback) {
-    request.get({
-        url: url,
-        json: true
-    }, function (err, resp, body) {
-        if (err) {
-            callback(err);
-        } else if (resp.statusCode !== 200) {
-            callback('Non-200 Response');
-        } else if (!body.result) { // expired token
-            doRefreshAccessToken(function (err) {
-                if (err) {
-                    callback(err);
-                } else {
-                    doGet(url.replace(/(access_token=)[^&]*([&|$])/, '$1' + access_token + '$2'), callback);
-                }
-            });
-        } else {
-            console.log(body.nextPageToken);
-            console.log(body.result.length);
-            callback(null, body.result, body.nextPageToken);
-        }
-    });
+function doGet (url, callback) {
+    logger.debug('GET ' + url.substr(0, 300) + '...');
+    async.retry({times: 12, interval: 5000}, function (retryCb) {
+        request.get({
+            url: url,
+            json: true,
+            timeout: 30000
+        }, function (err, resp, body) {
+            logger.debug('Got Response');
+            if (err) {
+                retryCb(err);
+            } else if (resp.statusCode !== 200) {
+                retryCb('Non-200 Response');
+            } else if (!body.result) { // expired token
+                logger.debug('Need to refresh access token');
+                doRefreshAccessToken(function (err) {
+                    if (err) {
+                        retryCb(err);
+                    } else {
+                        doGet(url.replace(/(access_token=)[^&]*([&|$])/, '$1' + access_token + '$2'), retryCb);
+                    }
+                });
+            } else {
+                retryCb(null, body.result, body.nextPageToken);
+            }
+        });
+    }, callback);
 }
 
-function doRefreshAccessToken(callback) {
-    console.log('Refreshing Access Token...');
+function doRefreshAccessToken (callback) { // this may get called multiple times in parallel if we are executing GETs in parallel, fine for now
+    logger.info('Refreshing Access Token...');
     refreshAccessToken(function (err, token) {
         if (err) {
             callback(err);
         } else {
             fs.writeFileSync('access_token.txt', token, { flag : 'w' });
             access_token = token;
+            logger.info('Current Access Token: ' + access_token);
             callback();
         }
     });
@@ -150,14 +154,19 @@ translateStream._transform = function (data, enc, next) {
     });
 };
 
+var i = 0;
+while (fs.existsSync('out_' + i + '.' + outputJsonOrCsv)) {
+    i++;
+}
+
 if (outputJsonOrCsv === "csv") {
     stream
     .pipe(translateStream)
-    .pipe(fs.createWriteStream('out.csv'));
+    .pipe(fs.createWriteStream('out_' + i + '.csv'));
 } else if (outputJsonOrCsv === "json") {
     stream
     .pipe(stringify())
-    .pipe(fs.createWriteStream('out.json'));
+    .pipe(fs.createWriteStream('out_' + i + '.json'));
 } else {
     throw new Error("Invalid outputJsonOrCsv specified.");
 }
